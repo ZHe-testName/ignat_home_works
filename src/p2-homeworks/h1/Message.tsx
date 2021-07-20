6daec3f5771ac794061136a30dbfb38e21ab5213
Zheka Horunzhii <horunzhy.wgen@gmail.com>
2021-07-20 20:27:50 +0300

    Added message component and some styles

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React from 'react';
+import classes from './Message.module.css';
 
-function Message() {
+type MessagePropsType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+};
+
+function Message(props: MessagePropsType) {
+    const {avatar, name, message, time} = props;
+
+    console.log(classes);
+ 
     return (
-        <div>
+        <div  className={classes.message}>
+            <div className={classes.avatar}>
+                    <img src={avatar} alt="avatar image" />
+            </div>
 
+            <div className={classes.textblock}>
+                <div className="text-field">
+                    <div className="client-name">
+                        {name}
+                    </div>
+
+                    <div className="message-text">
+                        {message}
+                    </div>
+                </div>
+                
+                <div className="time-field">
+                    {time}
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
-export default Message
+export default Message;
